5f96073c7ab8c8fcc464f58cf827f7c8fc229413
reef <Reeflevi@outlook.com>
2018-09-20 08:49:00 +0300

    hello

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// class Dog {
+
+//     contructor(){
+//         this.dog = 'lavrador';
+//         this.dogColor = ()=>{
+//             return 'red';
+//         };
+
+//     }
+// }
+
+// var dog = new Dog();
+// console.log(dog.dogColor());
+
+//more comments
+
+function Dog(_dog) {
+    this.dog = _dog;
+    this.dogType = ()=>{
+        this.dog;
+    };
+}
+
+var dogs = new Dog('lavrador');
+
+console.log(dogs.dogType);
